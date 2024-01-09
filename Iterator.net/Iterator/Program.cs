fbce0d7556e0d5eb7ef4e1db85c2a3b2ec19e54d
matthcol <matthias.colin@gmail.com>
2024-01-09 12:47:57 +0100

    project Iterator .Net

---
+++ Iterator.net/Iterator/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+IEnumerable<string> cities = new List<string>()
+{
+    "Toulouse",
+    "Pau"
+};
+
+IEnumerator<string> itCity = cities.GetEnumerator();
+Console.WriteLine(itCity.GetType());
+
+itCity.MoveNext();
+Console.WriteLine("City: " + itCity.Current);
+itCity.MoveNext();
+Console.WriteLine("City: " + itCity.Current);
+
+Console.WriteLine();
+
+foreach (string city in cities)
+{
+    Console.WriteLine(city);
+}
